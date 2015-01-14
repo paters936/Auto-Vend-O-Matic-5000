0e1992c75edb10079ec77e4e2ae7ef6ed91a3eae
Arthur Guy <arthur@assys.net>
2015-01-14 17:48:40 +0000

    refactored the coin reader

--- Arduino/Vendomatic/CoinReader.h
+++ Arduino/Vendomatic/CoinReader.h
@@ -1,10 +1,24 @@
 #include "Arduino.h"
+#include "pinDefinitions.h"
 
 class CoinReader {
-    public:
-        void setupPins();
-        void disable(int trueFalse);
-        
-    private:
-        
+  public:
+
+    void setupPins() {
+        pinMode(COIN_PIN3, INPUT_PULLUP);
+        pinMode(COIN_PIN4, INPUT_PULLUP);
+        pinMode(COIN_PIN5, INPUT_PULLUP);
+        pinMode(COIN_PIN6, OUTPUT);
+        pinMode(COIN_PIN7, INPUT_PULLUP);
+        pinMode(COIN_PIN8, INPUT_PULLUP);
+        pinMode(COIN_PIN9, INPUT_PULLUP);
+        pinMode(COIN_PIN10, INPUT_PULLUP);
+    }
+
+    void disable(int trueFalse) {
+        digitalWrite(COIN_PIN6, trueFalse);
+    };
+
+  private:
+
 };
