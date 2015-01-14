976278deebe2c5c72b7de496ba807a7b73924838
Arthur Guy <arthur@assys.net>
2015-01-14 20:24:41 +0000

    working on the buttons library - probbaly wont work

--- Arduino/Vendomatic/Vendomatic.ino
+++ Arduino/Vendomatic/Vendomatic.ino
@@ -1,9 +1,14 @@
 #include "pinDefinitions.h"
 #include "Dispenser.h"
 #include "CoinReader.h"
+#include "Buttons.h"
+
+//Core Libraries
+#include <Keypad.h>
 
 Dispenser dispenser;
 CoinReader coinReader;
+Buttons buttons;
 
 void setup() {
   
